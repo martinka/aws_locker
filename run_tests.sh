2703f1e3b28e24c8a792ad63831f4f57a0cfd705
Darby Mullen <darby.mullen@ntrepidcorp.com>
2019-09-19 16:07:13 -0500

    Added running unit tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,13 @@
 trap ': "*** BUILD FAILED ***" $BASH_SOURCE:$LINENO: error: "$BASH_COMMAND" returned $?' ERR
 set -eExuo pipefail
 
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8 
+
+# change into the directory that contains the script 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 cd src
 
 # run tests 
